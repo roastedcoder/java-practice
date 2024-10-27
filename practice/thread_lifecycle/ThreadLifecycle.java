1710c0917ef97e36ef881b6cffd87440cdf76056
Suraj Kumar <vermasuraj412@gmail.com>
2024-10-27 17:00:19 +0530

    feat: thread priority, daemon

--- practice/thread_lifecycle/ThreadLifecycle.java
+++ practice/thread_lifecycle/ThreadLifecycle.java
@@ -46,4 +46,37 @@ public class ThreadLifecycle {
     - MONITOR LOCK:
       - It helps to make sure that only 1 thread goes into the particular section of code(a synscronized block or method.)
   */
+
+  /*
+    Why STOP, RESUME, SUSPEND method is deprecated?
+      - STOP: Terminates the thread abruptly, No lock release, no resource clean up happens.
+      - SUSPEND: Put the thread on hold(suspend) for temporarily. No lock is released too.
+      - RESUME: Used to resume the execution of suspended thread.
+  */
+
+  /*
+    JOIN:
+    - When JOIN method is invoked on a thread object. Current thread will be blocked and waits for specific thread to finish.
+    - It is useful when we want to coordinate between threads or to ensure we complete certain task before moving ahead.
+  */
+
+  /*
+    Thread Priority:
+    - Priorities are integers ranging from 1(low) to 10(high).
+    - Even we set thread priority while creation, its not guaranteed to follow any specific order, its just a hint to thread
+      schedular which to execute next.(but its not strict rule)
+    - when new thread is created, it inherit the priority of its parent thread.
+    - we can set current priority using "setPriority(int priority)" method.
+    - ** avoid using thread priority.
+  */
+
+  /*
+    Deamon Thread:
+    - used for:
+      - GC
+      - Auto save
+      - logging
+    - create by passing "setDaemon: true" after creating the thread.
+    - it will terminate as the parent thread is terminated.
+  */
 }
--- practice/thread_lifecycle/producer_consumer_single/Main.java
+++ practice/thread_lifecycle/producer_consumer_single/Main.java
@@ -18,5 +18,44 @@ public class Main {
     // we can start producer thread first but producer thread already has wait of 5 second.
     consumeThread.start();
     produceThread.start();
+
+    /*
+      Output when JOIN is not used.
+      STDOUT:
+      **Main Thread completed it's task
+        Consume thread: Thread-1
+        Producer thread: Thread-0
+        ConsumeItem method invoked by: Thread-1
+        Thread Thread-1 is waiting now.
+        Item added by: Thread-0 and invoking all thread which are waiting.
+        Item consumed by: Thread-1
+
+      Main thread would not wait for other thread to complete it's task.
+
+      we use join() for each thread so that main thread can join them to complete their task on their own.
+    */
+
+    System.out.println("Main Thread completed it's task");
+
+    try{
+      produceThread.join();
+      consumeThread.join();
+    } catch(Exception e) {
+
+    }
+    System.out.println("Main Thread joined other 2 thread.");
+
+    /*
+    Output after using join on other 2 threads.
+      STDOUT:
+        Main Thread completed it's task
+        Producer thread: Thread-0
+        Consume thread: Thread-1
+        ConsumeItem method invoked by: Thread-1
+        Thread Thread-1 is waiting now.
+        Item added by: Thread-0 and invoking all thread which are waiting.
+        Item consumed by: Thread-1
+      **Main Thread joined other 2 thread.
+    */
   }
 }
