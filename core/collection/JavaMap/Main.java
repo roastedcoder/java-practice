c68c668e508e3bf1c20dc7e86ba54f775714dae5
Suraj Kumar <vermasuraj412@gmail.com>
2024-11-02 17:57:00 +0530

    feat: map: treemap

--- core/collection/JavaMap/Main.java
+++ core/collection/JavaMap/Main.java
@@ -1,6 +1,8 @@
 import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Main {
   public static void main(String[] args) {
@@ -12,9 +14,12 @@ public class Main {
           - synchronized version of HashMap
           - do not contains null key or value.
         - LinkedHashMap:
-          - Maintains the insertion order.
+          - Maintains the insertion order or
+          - helps in maintaining access order.
+          - it maintains a doubly linked list of keys while inserting the key so that order is maintained.
         - TreeMap:
-          - sorts the data internally.
+          - sorts the data internally according to its natural ordering or Comparator provided.
+          - It is based on Red-Black tree(self balancing BST).
       - can not contains duplicate `key`.
       - contains (key, value).
 
@@ -24,6 +29,7 @@ public class Main {
     */
 
     // HashTable
+    System.out.println("HashTable:");
     Hashtable<Integer, Integer> hashtable = new Hashtable<>();
     hashtable.put(1, 2);
     hashtable.put(3, 4);
@@ -36,6 +42,7 @@ public class Main {
     }
 
     // HashMap
+    System.out.println("HashMap:");
     Map<Integer, Integer> hashmap = new HashMap<>();
     hashmap.put(3, 4);
     hashmap.put(1, 2);
@@ -59,5 +66,51 @@ public class Main {
     for (Map.Entry entry : hashmap.entrySet()) {
         System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
     }
+
+    hashmap.forEach((Integer key, Integer val) -> System.out.println("Key: " + key + " and value: " + val));
+
+    // LinkedHashMap
+    System.out.println("LinkedHashMap:");
+    LinkedHashMap<Integer, Integer> linkedhashmap = new LinkedHashMap<>(16, .75f, true);
+    linkedhashmap.put(1, 2);
+    linkedhashmap.put(3, 4);
+    linkedhashmap.put(5, 6);
+
+    for (Map.Entry<Integer, Integer> entry : linkedhashmap.entrySet()) {
+        System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+    }
+
+    linkedhashmap.forEach((Integer key, Integer val) -> System.out.println("Key: " + key + " and value: " + val));
+
+    linkedhashmap.get(1);
+    // now (key: 1) will move to last
+    // We can use linkedhashmap in caching, purge the initial key, value.
+
+    linkedhashmap.forEach((Integer key, Integer val) -> System.out.println("Key: " + key + " and value: " + val));
+
+    System.out.println("TreeMap:");
+    // TreeMap<Integer, Integer> treemap = new TreeMap<>(Comparator.naturalOrder());
+    // TreeMap<Integer, Integer> treemap = new TreeMap<>((Integer k1, Integer k2) -> k2 - k1); // reverse
+    // TreeMap<Integer, Integer> treemap = new TreeMap<>((Integer k1, Integer k2) -> k1 - k2); // natural order
+    TreeMap<Integer, Integer> treemap = new TreeMap<>();
+    treemap.put(3, 4);
+    treemap.put(1, 2);
+    treemap.put(5, 6);
+    treemap.put(7, 8);
+    treemap.put(9, 10);
+
+    for (Map.Entry<Integer, Integer> entry : treemap.entrySet()) {
+        System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+    }
+
+    System.out.println("Headmap 5: " + treemap.headMap(5)); // exclusive
+    System.out.println("Headmap 5: " + treemap.tailMap(5)); // inclusive
+
+    System.out.println("LowerEntry 5 key: " + treemap.lowerEntry(5).getKey() + " value: " + treemap.lowerEntry(5).getValue());
+    System.out.println("LowerKey: " + treemap.lowerKey(5));
+
+    System.out.println("FloorEntry 5 key: " + treemap.floorEntry(5).getKey() + " value: " + treemap.floorEntry(5).getValue());
+    System.out.println("FloorKey: " + treemap.floorKey(5));
+    
   }
 }
